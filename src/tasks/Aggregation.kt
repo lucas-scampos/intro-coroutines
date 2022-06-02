09f09007b472c588391e9b587508ca339e1670eb
Lucas Campos <lucas.campos@uberall.com>
2022-06-02 09:54:11 +0200

    Lightning Talk Coroutines

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -2,17 +2,12 @@ package tasks
 
 import contributors.User
 
-/*
-TODO: Write aggregation code.
+fun List<User>.aggregate(): List<User>  {
+    var aggregated = mutableListOf<User>()
 
- In the initial list each user is present several times, once for each
- repository he or she contributed to.
- Merge duplications: each user should be present only once in the resulting list
- with the total value of contributions for all the repositories.
- Users should be sorted in a descending order by their contributions.
+    this.groupBy { it.login }.entries.forEach {
+        aggregated.add(User(it.key, it.value.fold(0) { sum, element -> sum + element.contributions}))
+    }
 
- The corresponding test can be found in test/tasks/AggregationKtTest.kt.
- You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
-*/
-fun List<User>.aggregate(): List<User> =
-    this
+    return aggregated.sortedBy { it.contributions }.reversed()
+}
